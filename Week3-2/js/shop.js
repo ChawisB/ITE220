e482044c4f7cb75f3dfcf5476c4fd09b2a18de67
DESKTOP-UORHM3V\Administrator <gem.bijaisoradat@gmail.com>
2017-04-07 18:43:01 +0700

    Fixed Week2

--- Week3-2/js/shop.js
+++ Week3-2/js/shop.js
@@ -1,17 +1,30 @@
-var price;
-var quantity;
-var Total;
+var products = [
+	"Brooklyn T-Shirt White", 
+	"Brooklyn T-Shirt Black", 
+	"Apple Watch", 
+	"Android Phone"
+];
+var prices = [10,10,199,159];
+
 var customerName = "Gem";
-
-price = 5;
-quantity = 14;
-Total = price*quantity;
-
+var total = 0;
 var customer = document.getElementById("name");
 customer.textContent = customerName;
 
 
-//document.write("Total is " + total);
+var productsText = "";
+var productsElement = document.getElementById("product-list");
 
-var total = document.getElementById("price");
-total.textContent = Total;
+for(var i = 0; i<products.length;i++) {
+
+	productsText += "<li class='list-group-item'><span class='badge'>$"+prices[i]+"</span>"+products[i]+"</li>";
+	total += prices[i];
+}
+
+
+productsElement.innerHTML = productsText;
+
+var totalPriceElement = document.getElementById("total-price");
+totalPriceElement.textContent = total;
+
+var date = new Date
